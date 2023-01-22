9483534907256560ef94bf03b1d85fb272296ee4
Sergey <119783324+SereyPSV@users.noreply.github.com>
2023-01-22 21:33:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,9 +38,22 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = ""; let a = ""; let a1 = ""; let a2 = ""; let exprS = ""; let expr1 = ""; let expr2 = ""; let i = 0; let x1 = 0; let x2 = 0; 
+    for (i=0; i < (expr.length / 10); i++) {
+        expr1 = "";
+        x1 = 10*i; x2 = x1 + 10;
+        a = expr.slice(x1, x2);
+        if (a != "**********") {
+        a1 = a.replaceAll("11", "-");
+        a2 = a1.replaceAll("10", ".");
+        exprS = a2.replaceAll("00", "");
+        expr1 = MORSE_TABLE[exprS];
+        } else {expr1 = " "}
+        expr2 = expr2 + expr1;
+    }
+    result = expr2;
+    return result;
 }
-
 module.exports = {
     decode
 }
